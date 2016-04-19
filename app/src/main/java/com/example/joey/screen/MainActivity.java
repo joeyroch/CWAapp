c23890ca729f74abf72c4abbeee09431f4d1b990
Joey <joeyroch@iastate.edu>
2016-04-19 01:07:51 -0500

    Test Commit

--- app/src/main/java/com/example/joey/screen/MainActivity.java
+++ app/src/main/java/com/example/joey/screen/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String MyPREFERENCES = "MyPrefs";
     SharedPreferences sharedpreferences;
 
-
+    //Test Commit
     TextView mond;
     TextView tuesd;
     TextView wed;
